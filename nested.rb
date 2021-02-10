e314436119ef32645c02c5a685facdc55512e2bd
Christopher Kim <chris.kim1990@gmail.com>
2021-02-10 21:13:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -101,7 +101,7 @@ def changing_alan
     }
 
 end
-  programmer_hash[:alan_kay][:known_for] = ["GUI"]
+  programmer_hash[:alan_kay][:known_for][0] = ["GUI"]
 
 def adding_to_dennis
   # add "Assembly" to Dennis Ritchie's languages array
